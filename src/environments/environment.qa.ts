3073277d0a338bbedee77b4cd40903a871984aa8
samjm <samjm@MacBook-Pro-de-samjm.local>
2019-01-16 22:23:44 -0600

    automatic docker build

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -8,11 +8,11 @@
 
 export const environment = {
   production: false,
-  backend_sdk: 'https://api.greenroutesdk.com.mx/back-sdk',
+  backend_sdk: 'GR_BACKEND_URL',
   backend_orion: 'https://www.smartsdkitesm.com:1027',
   backend_encryption: 'https://www.smartsdkitesm.com:3001',
   backend_api: 'https://www.smartsdkitesm.com:3002',
   encrypt: false,
   infotec_write_location:"orion",
-  infotec_backend_orion: "https://orion.greenroutesdk.com.mx"
+  infotec_backend_orion: 'GR_ORION_URL'
 };
